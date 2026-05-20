a7f699d78f0951b7de2b09809307e081697872d8
Mittal1225-New <mittalgadhiya25@gmail.com>
2026-05-20 14:48:36 -0500

    Add files via upload

---
+++ Selenium2022Sessions/src/test/java/SeleniumBasics/WebDriverBasics.java
@@ -0,0 +1,54 @@
+
+package SeleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverBasics {
+
+	public static void main(String[] args) {
+		
+		
+		
+		//if you run this it will give java.lang.IllegalStateException
+		
+
+		WebDriverManager.chromedriver().setup();
+		
+		
+		//System.setProperty("webdriver.chrome.driver","/Users/drmga/Downloads/chromedriver-mac-arm64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		driver.get("https://www.amazon.com/");
+		driver.getCurrentUrl();
+		
+		String title = driver.getTitle();
+		
+
+		System.out.println("page title is : " + title);
+		
+		
+		//checkpoint/verification Point
+		
+		if(title.equals("google")){
+			System.out.println("correct");
+			
+	    }else
+		
+		System.out.println("incorrect");
+		
+			
+		System.out.println(driver.getCurrentUrl());
+		
+		
+		if(driver.getCurrentUrl().contains("google")) {
+			System.out.println("url is correct");
+		}
+		 
+	}
+
+}
